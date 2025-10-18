6a4daf868908315733b100877d221a2aa58ef240
LePhanHaiLong <longlph.23it@gmail.com>
2025-10-18 21:49:19 +0700

    convert module question

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Question } from './entities/question.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Question
+    ]),
+  ],
   controllers: [QuestionController],
   providers: [QuestionService],
+  exports: [QuestionService],
 })
-export class QuestionModule {}
+export class QuestionModule { }

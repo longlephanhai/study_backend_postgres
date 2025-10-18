f89e25aa9ed536882b5960e699a7f746ff5878ee
LePhanHaiLong <longlph.23it@gmail.com>
2025-10-18 23:15:27 +0700

    convert module writing

--- src/writing/dto/create-writing.dto.ts
+++ src/writing/dto/create-writing.dto.ts
@@ -1,1 +1,25 @@
-export class CreateWritingDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateWritingDto {
+  @IsNotEmpty({ message: "Topic is required" })
+  topic: string;
+
+  @IsNotEmpty({ message: "Title is required" })
+  title: string;
+
+  @IsNotEmpty({ message: "Description is required" })
+  description: string;
+
+  @IsNotEmpty({ message: "Minimum words are required" })
+  minWords: number;
+
+  @IsNotEmpty({ message: "Maximum words are required" })
+  maxWords: number;
+
+  @IsNotEmpty({ message: "Level is required" })
+  level: string;
+
+  @IsNotEmpty({ message: "Suggestion is required" })
+  suggestion: string;
+
+}
--- src/writing/writing.controller.ts
+++ src/writing/writing.controller.ts
@@ -1,25 +1,37 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { WritingService } from './writing.service';
 import { CreateWritingDto } from './dto/create-writing.dto';
 import { UpdateWritingDto } from './dto/update-writing.dto';
+import { ResponseMessage } from 'src/decorator/customize';
 
 @Controller('writing')
 export class WritingController {
-  constructor(private readonly writingService: WritingService) {}
+  constructor(private readonly writingService: WritingService) { }
 
   @Post()
   create(@Body() createWritingDto: CreateWritingDto) {
     return this.writingService.create(createWritingDto);
   }
 
+  @Post('multiple')
+  @ResponseMessage('Create multiple writings successfully')
+  createMultiple(@Body() createWritingDto: CreateWritingDto[]) {
+    return this.writingService.createMultiple(createWritingDto);
+  }
+
   @Get()
-  findAll() {
-    return this.writingService.findAll();
+  @ResponseMessage("Get all writings with pagination")
+  findAll(
+    @Query("current") currentPage: string,
+    @Query("pageSize") limit: string,
+    @Query() qs: string) {
+    return this.writingService.findAll(+currentPage, +limit, qs);
   }
 
   @Get(':id')
+  @ResponseMessage("Get writing successfully")
   findOne(@Param('id') id: string) {
-    return this.writingService.findOne(+id);
+    return this.writingService.findOne(id);
   }
 
   @Patch(':id')
